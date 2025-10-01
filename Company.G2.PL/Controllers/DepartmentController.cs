20e81e702eb32b2fcad86c68c37d67864b0869ff
user <user@DESKTOP-0FTCJBS>
2025-10-01 09:52:43 +0300

    Create Create Action-View

--- Company.G2.PL/Controllers/DepartmentController.cs
+++ Company.G2.PL/Controllers/DepartmentController.cs
@@ -1,5 +1,7 @@
 ﻿using Company.G2.BLL.Interfaces;
 using Company.G2.BLL.Repositories;
+using Company.G2.DAL.Models;
+using Company.G2.PL.Dtos;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Company.G2.PL.Controllers
@@ -22,5 +24,36 @@ namespace Company.G2.PL.Controllers
            
             return View(departments);
         }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+       
+        [HttpPost]
+        public IActionResult Create(CreateDepartmentDto model)
+        {
+
+            if (ModelState.IsValid) //Server Side Validation 
+            {
+                var department = new Department()
+                {
+                    Code=model.Code,
+                    Name=model.Name,
+                    CreateAt=model.CreateAt
+                };
+              var count =  _departmentRepository.Add(department);
+
+                if (count > 0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+
+            }
+            return View(model);
+        }
+
+
     }
 }
---
+++ Company.G2.PL/Dtos/CreateDepartmentDto.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+using RequiredAttribute = System.ComponentModel.DataAnnotations.RequiredAttribute;
+
+namespace Company.G2.PL.Dtos
+{
+    public class CreateDepartmentDto
+    {
+        [Required(ErrorMessage = "Code is Required !")]
+        public string Code { get; set; }
+
+        [Required(ErrorMessage = "Name is Required !")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "CreateAt is Required !")]
+        public DateTime CreateAt { get; set; }
+    }
+}
